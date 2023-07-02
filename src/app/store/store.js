14a779d83b546934c4677e9d0da3d76ec96a385b
Volodymyr Kopynets <linch433@gmail.com>
2023-07-02 19:41:39 +0300

    Added authSlice with counter for redux toolkit, that actually doesn't work

--- src/app/store/store.js
+++ src/app/store/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import { api } from './features/api.js';
 import { setupListeners } from '@reduxjs/toolkit/query/react';
 import { createLogger } from 'redux-logger';
+import { counterSlice } from '../store/slices/authSlice.js';
 
 const logger = createLogger({
   collapsed: true,
@@ -9,6 +10,7 @@ const logger = createLogger({
 
 export const store = configureStore({
   reducer: {
+    counter: counterSlice,
     [api.reducerPath]: api.reducer,
   },
   middleware: (getDefaultMiddleware) =>
